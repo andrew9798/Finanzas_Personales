5c2a48f9925794d126409f085330e82b478ec4e5
andgan <andresganga1298@gmail.com>
2025-10-18 00:50:49 +0200

    creacion primeros endpoints

--- frontend/src/services/gastos.js
+++ frontend/src/services/gastos.js
@@ -1,25 +1,38 @@
 import axios from 'axios'
 const baseUrl = 'http://localhost:1234/gastos'
 
+
+//funciones para obtener todos los gastos, crear, eliminar y actualizar
 const getAll = () => {
   const request = axios.get(baseUrl)
   return request.then(response => response.data)
 }
 
+//funcion para crear nuevo gasto
 const create = newObject => {
   const request = axios.post(baseUrl, newObject)
   return request.then(response => response.data)
 }
 
+//funcion para eliminar gasto
 const Delete = id => {
   const request = axios.delete(`${baseUrl}/${id}`)
   return request.then(response => response.data)
 } 
 
+//funcion para actualizar gasto
 const update = (id, newObject) => {
   const request = axios.put(`${baseUrl}/${id}`, newObject)
   return request.then(response => response.data)
 }
 
+//crear función que encuentre gastos por mes y año, teniendo en cuenta que la fecha es un string en formato "YYYY-MM-DD"   
+const findByMonthYear = (mes, anyo) => {
+  const request = axios.get(`${baseUrl}/gastos/${anyo}/${mes}`)
+  console.log(request)
+  return request.then(response => response.data)
+  
+}
 
-export default { getAll, create, Delete, update }
+
+export default { getAll, create, Delete, update, findByMonthYear }
--- frontend/src/services/ingresos.js
+++ frontend/src/services/ingresos.js
@@ -1,21 +1,25 @@
 import axios from 'axios'
 const baseUrl = 'http://localhost:1234/ingresos'
 
+//funciones para obtener todos los ingresos
 const getAll = () => {
   const request = axios.get(baseUrl)
   return request.then(response => response.data)
 }
 
+//funcion para crear nuevo ingreso
 const create = newObject => {
   const request = axios.post(baseUrl, newObject)
   return request.then(response => response.data)
 }
 
+//funcion para eliminar ingreso
 const Delete = id => {
   const request = axios.delete(`${baseUrl}/${id}`)
   return request.then(response => response.data)
 } 
 
+//funcion para actualizar ingreso
 const update = (id, newObject) => {
   const request = axios.put(`${baseUrl}/${id}`, newObject)
   return request.then(response => response.data)
@@ -23,7 +27,7 @@ const update = (id, newObject) => {
 } 
 //crear función que encuentre ingresos por mes y año, teniendo en cuenta que la fecha es un string en formato "YYYY-MM-DD"   
 const findByMonthYear = (mes, anyo) => {
-  const request = axios.get(`${baseUrl}?mes=${mes}&anyo=${anyo}`)
+  const request = axios.get(`${baseUrl}/ingresos/${anyo}/${mes}`)
   console.log(request)
   return request.then(response => response.data)
   
